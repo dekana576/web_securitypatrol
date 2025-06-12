9498765e48d245c526c248003107b031b99574c2
dekana576 <dekana576@gmail.com>
2025-06-12 13:57:29 +0800

    membuat seeder admin

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin Utama',
+            'username' => 'admin',
+            'nik' => '1234567890123456',
+            'phone_number' => '081234567890',
+            'gender' => 'male',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('12345678'), // ubah sesuai kebutuhan
+            'sales_office_id' => 1, // pastikan ada sales office dengan ID 1
+            'role' => 'admin', // sesuaikan dengan role yang kamu pakai
+        ]);
+    }
+}
