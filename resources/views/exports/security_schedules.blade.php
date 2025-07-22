816606d3f1d4f1ab5390a88099346938a4886bfe
dekana576 <dekana576@gmail.com>
2025-07-22 21:20:07 +0800

    menambahkan kolom nomor pada print jadwal

--- resources/views/exports/security_schedules.blade.php
+++ resources/views/exports/security_schedules.blade.php
@@ -34,7 +34,9 @@
     </style>
 </head>
 <body>
-    <h1>Jadwal Patroli Security</h1>
+
+        <h1 style="text-align: center; margin-bottom: 50px">Jadwal Patroli Security</h1>
+
     <h3>
         Region: {{ $region->name }} <br>
         Sales Office: {{ $salesOffice->sales_office_name }} <br>
@@ -42,65 +44,69 @@
     </h3>
 
     <table>
-        <thead>
-            <tr>
-                <th rowspan="2">Nama Security</th>
-                @foreach($dates as $date)
-                    @php $isSunday = $date->locale('id')->translatedFormat('D') === 'Min'; @endphp
-                    <th class="{{ $isSunday ? 'bg-sunday' : '' }}">
-                        {{ $date->format('d') }}
-                    </th>
-                @endforeach
-            </tr>
-            <tr>
-                @foreach($dates as $date)
-                    @php $isSunday = $date->locale('id')->translatedFormat('D') === 'Min'; @endphp
-                    <th class="{{ $isSunday ? 'bg-sunday' : '' }}">
-                        {{ $date->locale('id')->translatedFormat('D') }}
-                    </th>
-                @endforeach
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($dataPerSecurity as $securityName => $dataHarian)
-                <tr>
-                    <td style="text-align: left">{{ $securityName }}</td>
-                    @foreach($dates as $date)
-                        @php $val = $dataHarian[$date->format('Y-m-d')] ?? ''; @endphp
-                        <td>
-                            @switch($val)
-                                @case('p')
-                                    <span style="display:inline-block; padding:2px 6px; font-size:9px; font-weight:bold; border-radius:4px; background-color:#198754; color:black;">
-                                        P
-                                    </span>
-                                    @break
-
-                                @case('s')
-                                    <span style="display:inline-block; padding:2px 6px; font-size:9px; font-weight:bold; border-radius:4px; background-color:#ffc107; color:black;">
-                                        S
-                                    </span>
-                                    @break
-
-                                @case('m')
-                                    <span style="display:inline-block; padding:2px 6px; font-size:9px; font-weight:bold; border-radius:4px; background-color:#343a40; color:black;">
-                                        M
-                                    </span>
-                                    @break
-
-                                @case('p8')
-                                    <span style="display:inline-block; padding:2px 6px; font-size:9px; font-weight:bold; border-radius:4px; background-color:#f8d7da; color:black;">
-                                        P8
-                                    </span>
-                                    @break
-
-                                @default
-                                    <span style="color:#dc3545;">OFF</span>
-                            @endswitch
-                        </td>
-                    @endforeach
-                </tr>
+    <thead>
+        <tr>
+            <th rowspan="2">No</th> <!-- Tambahan -->
+            <th rowspan="2">Nama Security</th>
+            @foreach($dates as $date)
+                @php $isSunday = $date->locale('id')->translatedFormat('D') === 'Min'; @endphp
+                <th class="{{ $isSunday ? 'bg-sunday' : '' }}">
+                    {{ $date->format('d') }}
+                </th>
             @endforeach
-        </tbody>
-    </table>
+        </tr>
+        <tr>
+            @foreach($dates as $date)
+                @php $isSunday = $date->locale('id')->translatedFormat('D') === 'Min'; @endphp
+                <th class="{{ $isSunday ? 'bg-sunday' : '' }}">
+                    {{ $date->locale('id')->translatedFormat('D') }}
+                </th>
+            @endforeach
+        </tr>
+    </thead>
+    <tbody>
+        @php $no = 1; @endphp <!-- Tambahkan counter -->
+        @foreach($dataPerSecurity as $securityName => $dataHarian)
+            <tr>
+                <td>{{ $no++ }}</td> <!-- Kolom nomor urut -->
+                <td style="text-align: left">{{ $securityName }}</td>
+                @foreach($dates as $date)
+                    @php $val = $dataHarian[$date->format('Y-m-d')] ?? ''; @endphp
+                    <td>
+                        @switch($val)
+                            @case('p')
+                                <span style="display:inline-block; padding:2px 6px; font-size:9px; font-weight:bold; border-radius:4px; background-color:#198754; color:black;">
+                                    P
+                                </span>
+                                @break
+
+                            @case('s')
+                                <span style="display:inline-block; padding:2px 6px; font-size:9px; font-weight:bold; border-radius:4px; background-color:#ffc107; color:black;">
+                                    S
+                                </span>
+                                @break
+
+                            @case('m')
+                                <span style="display:inline-block; padding:2px 6px; font-size:9px; font-weight:bold; border-radius:4px; background-color:#343a40; color:black;">
+                                    M
+                                </span>
+                                @break
+
+                            @case('p8')
+                                <span style="display:inline-block; padding:2px 6px; font-size:9px; font-weight:bold; border-radius:4px; background-color:#f8d7da; color:black;">
+                                    P8
+                                </span>
+                                @break
+
+                            @default
+                                <span style="color:#dc3545;">OFF</span>
+                        @endswitch
+                    </td>
+                @endforeach
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+
 </body>
 </html>
